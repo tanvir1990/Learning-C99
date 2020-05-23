dff276a66435a2d02d48bc995dd2b316574656aa
tanvir1990 <pavel.mhp@gmail.com>
2020-05-23 18:22:42 -0400

    Exercise Working Version

--- Lab 4/lab4.c
+++ Lab 4/lab4.c
@@ -13,37 +13,45 @@ void configure_PORT1 (void){
 	P1REN  |= (uint8_t) ((1<<4) | (1<<1));											//P1.1 and P1.4 swtiches need internal resistor, pull-up, set the bits to 1
 }
 
+
 /*
 This function configures pin 1 and 4 of PORT1 to enbale Interrupts 
 */
-void configure_Interrupts(){
-	P1IES |= (uint8_t) ((1<<4) | (1<<1));											//Edge Select, for switches we set the bits
-	P1IFG &= (uint8_t) ~((1<<4) | (1<<1));											//Clear the previous flags
-	P1IE  |= (uint8_t) ~((1<<4) | (1<<1));											//Enable Interrupts by setting pin 1 and 4 to 1
+//void configure_Port1_Interrupts(){
+//	P1IES |= (uint8_t) ((1<<4) | (1<<1));											//Edge Select, for switches we set the bits
+//	P1IFG &= (uint8_t) ~((1<<4) | (1<<1));											//Clear the previous flags
+//	P1IE  |= (uint8_t) ~((1<<4) | (1<<1));											//Enable Interrupts by setting pin 1 and 4 to 1
+//	
+//																					//Configure NVIC
+//	NVIC_SetPriority(PORT1_IRQn, 2);												//Encode the prioirity as 2
+//	NVIC_ClearPendingIRQ(PORT1_IRQn);												//Clear previous IRQ
+//	NVIC_EnableIRQ(PORT1_IRQn);														//Enable Interrupt
+//}
+
+
+
+
+//void configure_PORT2(void){
+//	P2SEL0 &= (uint8_t) (~((1<<2) | (1<<1) | (1<<0)));								//Clearing bits 0,1,2, to set them as GPIO
+//	P2SEL1 &= (uint8_t) (~((1<<2) | (1<<1) | (1<<0)));								//Clearing bits 0,1,2
+//	P2DIR  |= (uint8_t) ( (1<<2)  | (1<<1) | (1<<0) );								//P2.0, 2.1, 2.2 are ouputs, Set the DIR register for OUTPUT by setting the bits to1
+//	P2OUT  &= (uint8_t) (~((1<<2) | (1<<1) | (1<<0)));								//P2.0, 2.1, 2.2 aer LED and Active high, so clear the bits 0
+//}
+
+
+
+void configure_TimerA_Interrupts(){
+	NVIC_SetPriority(TA0_N_IRQn, 3);
+	NVIC_ClearPendingIRQ(TA0_N_IRQn);
+	NVIC_EnableIRQ(TA0_N_IRQn);
 	
-																					//Configure NVIC
-	NVIC_SetPriority(PORT1_IRQn, 2);												//Encode the prioirity as 2
-	NVIC_ClearPendingIRQ(PORT1_IRQn);												//Clear previous IRQ
-	NVIC_EnableIRQ(PORT1_IRQn);														//Enable Interrupt
-																	//Enable Interrupt Globally
-	
+	__ASM("CPSIE I");
 }
 
 
 
-
-
-/*ISR for Timer Interrupts*/
-void TA0_N_IRQHandler(void){			
-	TA0CTL &= (uint16_t) ~ (BIT0);	//Clear the TAIFG flag
-	P1OUT ^= (uint8_t) BIT0;
-}
-int main (){
+void configure_TA0CTL_bits(){
 	
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;							//Disbale Watchdog Timer
-
-	configure_PORT1();																			//Call the function to configure Port1
-
 	TA0CTL &=~(uint16_t)(BIT0); 														//BIT 0 = 0 for TAIFG flag
 	TA0CTL |= (uint16_t)(BIT1); 														//BIT 1 For enabling interrupt
 	TA0CTL |= (uint16_t)(BIT2);															//BIT 2, Reset the counter
@@ -56,10 +64,37 @@ int main (){
 	TA0CTL |= (uint16_t) (BIT8);	
 
 	TA0EX0 &=~ (uint16_t) ((BIT2) | (BIT1)| (BIT0));  			// 000 divide by 1
-	TA0CCR0 = (uint16_t) (32768 - 1);																			
+	TA0CCR0 = (uint16_t) (32768 - 1);	
+}
+
+//void PORT1_IRQHandler(){
+//	
+//	if(!(P1IN & (uint8_t) (BIT1))){													// Button 1 is pressed
+//	P1IFG &= (uint8_t) ~((1<<4) | (1<<1));
+//	P1OUT &=~ (uint8_t) BIT0;
+//	P2OUT |= (uint8_t) BIT0;
+//	}
+//}
+
+
+/*ISR for Timer Interrupts*/
+void TA0_N_IRQHandler(void){			
+	TA0CTL &= (uint16_t) ~ (BIT0);	//Clear the TAIFG flag
+	P1OUT ^= (uint8_t) BIT0;
+}
+
+
+int main (){
 	
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;							//Disbale Watchdog Timer
+
+	configure_PORT1();																			//Call the function to configure Port1
+	//configure_PORT2();	
+	configure_TA0CTL_bits();
 	
-	__ASM("CPSIE I");
+	//configure_Port1_Interrupts();
+	configure_TimerA_Interrupts();
+	
 	while (1){																							
 	__ASM("WFI");																						
 	}																												
