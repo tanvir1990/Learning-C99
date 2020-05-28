711f7d303cea5b02fd537673b3f2c3ba22865702
tanvir1990 <pavel.mhp@gmail.com>
2020-05-28 01:42:39 -0400

    Included init and set funcs, buzzer working

--- Lab 5/lab5.c
+++ Lab 5/lab5.c
@@ -3,29 +3,37 @@
 #include "../inc/CortexM.h"
 #include "../inc/msp432p401r.h"
 
-//void BSP_buzzer_init(uint16_t duty){
-//	static uint32_t SubsystemFrequency = 3000000;
-//	static uint16_t PWMCycles;
-//	//uint16_t tone_frequency = pwm;
-//  if(duty > 1023){
-//    return;                        // invalid input
-//  }
-//  TA0CTL &= ~0x0030;               // halt Timer A0
+static uint32_t SubsystemFrequency = 3000000; // cycles/second
+static uint32_t PWMCycles;  
+static uint32_t tone_frequency = 2048;
 
-//  TA0CTL = 0x0200;
 
-//  TA0CCTL4 = 0x00E0;
-//  PWMCycles = SubsystemFrequency / 2048;
-//  TA0CCR4 = (duty*PWMCycles)>>10;  // defines when output signal is cleared
-//  TA0CCR0 = PWMCycles - 1;         // defines when output signals are set
-//  TA0EX0 &= ~0x0007;               // configure for input clock divider /1
-//  TA0CTL |= 0x0014;                // reset and start Timer A0 in up mode
-//  // ***************** GPIO initialization *****************
-//  P2SEL0 |= 0x80;
-//  P2SEL1 &= ~0x80;                 // configure P2.7 as timer out
-//  P2DIR |= 0x80;                   // make P2.7 out
-//}
+void BSP_buzzer_init(uint16_t duty){
+  if(duty > 1023){
+    return;                        // invalid input
+  }
+  TA0CTL &= ~0x0030;               // halt Timer A0
 
+  TA0CTL = 0x0200;
+
+  TA0CCTL4 = 0x00E0;
+  PWMCycles = SubsystemFrequency/tone_frequency;
+  TA0CCR4 = (duty*PWMCycles)>>10;  // defines when output signal is cleared
+  TA0CCR0 = PWMCycles - 1;         // defines when output signals are set
+  TA0EX0 &= ~0x0007;               // configure for input clock divider /1
+  TA0CTL |= 0x0014;                // reset and start Timer A0 in up mode
+  // ***************** GPIO initialization *****************
+  P2SEL0 |= 0x80;
+  P2SEL1 &= ~0x80;                 // configure P2.7 as timer out
+  P2DIR |= 0x80;                   // make P2.7 out
+}
+
+void BSP_buzzer_set(uint16_t duty){
+  if(duty > 1023){
+    return;                        // invalid input
+  }
+  TA0CCR4 = (duty*PWMCycles)>>10;  // defines when output signal is cleared
+}
 
 int main (){
 //	//Part 1
@@ -46,8 +54,8 @@ int main (){
 //	
 //	
 //	BSP_buzzer_init(512);
-	BSP_Buzzer_Init(50);
-	BSP_Buzzer_Set(0);
+	BSP_buzzer_init(50);
+	BSP_buzzer_set(0);
 	return 0;
 	
 }
